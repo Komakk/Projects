c7f1f478e8f201c079de9b6e76b61678469616ad
Anton Mikhailov <komakkomakk@gmail.com>
2021-11-25 18:27:21 +0400

    first app

---
+++ app/src/main/java/com/anton/android/geoquiz/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.anton.android.geoquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+    private lateinit var nextButton: Button
+    private lateinit var questionTextView: TextView
+
+    private val questionBank = listOf(
+            Qustion(R.string.question_australia, true),
+            Qustion(R.string.question_ocean, true),
+            Qustion(R.string.question_mideast, false),
+            Qustion(R.string.question_africa, false),
+            Qustion(R.string.question_americas, true),
+            Qustion(R.string.question_asia, true))
+
+    private var currentIndex = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+        nextButton = findViewById(R.id.next_button)
+        questionTextView = findViewById(R.id.question_text_view)
+
+        questionTextView.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+        trueButton.setOnClickListener { view: View ->
+            checkAnswer(true)
+        }
+        falseButton.setOnClickListener { view: View ->
+           checkAnswer(false)
+        }
+        nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+        updateQuestion()
+    }
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
+    }
+
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+        val messageResId = if (userAnswer == correctAnswer) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+        Toast.makeText(this,messageResId,Toast.LENGTH_SHORT).show()
+    }
+}
---
+++ app/src/main/java/com/anton/android/geoquiz/Qustion.kt
@@ -0,0 +1,5 @@
+package com.anton.android.geoquiz
+
+import androidx.annotation.StringRes
+
+data class Qustion(@StringRes val textResId: Int, val answer: Boolean)
